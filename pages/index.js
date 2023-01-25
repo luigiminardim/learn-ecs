e2b73bf80b4168390d77bc3651cce2606c5c0eb3
Luigi Minardi <luigiminardim@gmail.com>
2023-01-25 13:35:09 -0300

    feat: :sparkles: Add hostname.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ hostname }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +13,18 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js</a> on Luigi's Docker!
         </h1>
+        <p>You are at {hostname}</p>
       </main>
     </div>
   )
+}
+
+/** Get the hostname */
+export const getServerSideProps = async (context) => {
+  const hostname = context.req.headers.host;
+  return {
+    props: {
+      hostname,
+    },
+  }
 }
